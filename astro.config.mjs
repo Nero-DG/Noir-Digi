9e8488a040d595de271383c18afcd752c3505d1a
Nero-DG <giraudvincent49@gmail.com>
2026-08-29 17:50:40 +0200

    Remove remarkPoemBreak from markdown plugins
    Removed remarkPoemBreak plugin from markdown configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
-import remarkPoemBreak from './src/remark/remark-poem-break.js';
 
 export default defineConfig({
   site: 'https://nero-dg.github.io',
   base: '/Noir-Digi',
-  markdown: {
-    remarkPlugins: [remarkPoemBreak],
-  },
 });
